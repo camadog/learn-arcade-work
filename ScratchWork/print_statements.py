062df803e7d8c957d81088bd8849ec68ef2b265b
camadog <deadwar@gmail.com>
2020-10-01 18:10:40 -0500

    Lab 01

---
+++ ScratchWork/print_statements.py
@@ -0,0 +1,21 @@
+# Hello World
+print("Single statement")
+print("Hello World!\n")
+# Multiple lines
+# 1
+print("Multiple option 1")
+print("It was a dark and stormy night,")
+print("suddenly a shot rang out!\n")
+# 2
+print("Multiple option 2")
+print("line 1, \nline 2,\nline 3,\n")
+# 3
+print("Multiple option 3")
+print("""triple qoutes,
+line 2,
+line 3,
+""")
+# Escape
+print("Escape characters")
+print("Hi! This is a double quote mark: \"We use it for printing.\"")
+print("More escape characters: Single quote \', double quote \", tab \t, carrier return \r, linefeed \n,")
