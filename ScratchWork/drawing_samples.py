a3111bea7bd3c1c50bf5052a4fe3317ffd3a3adb
camadog <deadwar@gmail.com>
2020-10-04 00:12:09 -0500

    Lab 02

---
+++ ScratchWork/drawing_samples.py
@@ -0,0 +1,88 @@
+"""
+This is a sample program.
+
+Multi-line comments are surround by three double-quotes.
+"""
+
+# This is a single line comment.
+# Import the "arcade" library
+
+import arcade
+
+arcade.open_window(600, 600, "Drawing Example")
+
+# Set the background color
+arcade.set_background_color(arcade.csscolor.SKY_BLUE)
+# arcade.set_background_color((189, 55, 180))
+
+# Get ready to draw
+arcade.start_render()
+
+# Drawing coding goes here
+# Draw a rectangle
+# Left of 0, right of 599
+# Top of 300, bottom of 0
+arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, arcade.csscolor.GREEN)
+
+"""
+Tree trunk
+Center of 100, 320
+width of 20
+height of 60
+"""
+arcade.draw_rectangle_filled(100, 320, 20, 60, arcade.csscolor.SIENNA)
+# Tree top
+arcade.draw_circle_filled(100, 350, 30, arcade.csscolor.DARK_GREEN)
+
+# Another tree, with an ellipse for the top
+arcade.draw_rectangle_filled(200, 320, 20, 60, arcade.csscolor.SIENNA)
+arcade.draw_ellipse_filled(200, 370, 60, 80, arcade.csscolor.DARK_GREEN)
+
+"""
+Another tree, with a trunk and arc for the Top
+Arc is centered at (300, 340) with a width of 60 and height of 100
+The starting angle is 0, and ending angle is 180
+"""
+arcade.draw_rectangle_filled(300, 320, 20, 60, arcade.csscolor.SIENNA)
+arcade.draw_arc_filled(300, 340, 60, 100, arcade.csscolor.DARK_GREEN, 0, 180)
+
+"""
+Another tree, with a trunk and triangle for the Top
+Triangle is made of these points:
+(400, 400), (370, 320), (430, 320)
+"""
+arcade.draw_rectangle_filled(400, 320, 20, 60, arcade.csscolor.SIENNA)
+arcade.draw_triangle_filled(400, 400, 370, 320, 430, 320, arcade.csscolor.DARK_GREEN)
+
+# Draw a tree using a polygon with a list of points
+arcade.draw_rectangle_filled(500, 320, 20, 60, arcade.csscolor.SIENNA)
+arcade.draw_polygon_filled(((500, 400),
+                            (480, 360),
+                            (470, 320),
+                            (530, 320),
+                            (520, 360)),
+                            arcade.csscolor.DARK_GREEN)
+
+# Draw a sun
+arcade.draw_circle_filled(500, 550, 40, arcade.color.YELLOW)
+
+# Rays to the left, right, up and down
+arcade.draw_line(500, 550, 400, 550, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 600, 550, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 500, 450, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 500, 600, arcade.color.YELLOW, 3)
+
+# Diagonal rays
+arcade.draw_line(500, 550, 550, 600, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 550, 500, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 450, 600, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 450, 500, arcade.color.YELLOW, 3)
+
+# Drawing some text
+arcade.draw_text("Arbor Day - Plant a Tree!", 150, 230, arcade.color.BLACK, 24)
+
+# Finish Drawing
+arcade.finish_render()
+
+# Keep the window open
+arcade.run()
